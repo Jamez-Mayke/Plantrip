1e39392bc980d36bf12dfd2d2df8442123c18590
jamesmayke <jamezmayke28@gmail.com>
2024-09-11 10:48:06 -0300

    Init project Plantrip

---
+++ src/main/java/br/com/jamesmayke/plantrip/PlantripApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamesmayke.plantrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jamesmayke/plantrip/PlantripApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamesmayke.plantrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
