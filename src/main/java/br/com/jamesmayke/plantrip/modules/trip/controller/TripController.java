6037a52b1bbee0a88f42dcd850318e42d8959f26
jamesmayke <jamezmayke28@gmail.com>
2024-09-11 14:29:10 -0300

    Created endpoints of trip

--- src/main/java/br/com/jamesmayke/plantrip/modules/trip/controller/TripController.java
+++ src/main/java/br/com/jamesmayke/plantrip/modules/trip/controller/TripController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.jamesmayke.plantrip.modules.trip.dto.TripRequestPayload;
 import br.com.jamesmayke.plantrip.modules.trip.dto.TripResponseData;
 import br.com.jamesmayke.plantrip.modules.trip.dto.TripResponsePayload;
+import br.com.jamesmayke.plantrip.modules.trip.dto.TripUpdateRequest;
 import br.com.jamesmayke.plantrip.modules.trip.entity.Trip;
 import br.com.jamesmayke.plantrip.modules.trip.repository.TripRepository;
 
@@ -54,6 +56,37 @@ public class TripController {
 
         return ResponseEntity.notFound().build();
         
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripUpdateRequest payload) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip tripRaw = trip.get();
+            
+            tripRaw.setDestination(payload.destination());
+            tripRaw.setStartsAt(payload.starts_at());
+            tripRaw.setEndsAt(payload.ends_at());
+
+            this.repository.save(tripRaw);
+
+            return ResponseEntity.ok(tripRaw);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/confirm")
+    public void confirmTrip(@PathVariable UUID id) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if (trip.isPresent()) {
+            Trip tripRaw = trip.get();
+            tripRaw.setIsConfirmed(true);
+
+            this.repository.save(tripRaw);
+        }
     }
 
 }
